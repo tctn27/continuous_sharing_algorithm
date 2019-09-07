b30c9b08f7c1ab7aa37f28bbbe48c9749f074e0b
Tom Dunbavan <tomcatthenommer@gmail.com>
2019-09-07 19:36:30 +1000

    added a rounding option

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import os
+import math
 
 num_people = int(input("How many people are in the group? "))
 damage = int(input("What's the total damage? "))
 group_total = 0
+
+rounding = False
+if input("Would you like to use rounding? ").lower() == "yes":
+    rounding = True
 
 for player_instance in range(1, num_people + 1):
     print("\nPlayer " + str(player_instance))
@@ -10,17 +15,20 @@ for player_instance in range(1, num_people + 1):
     for iteration in range(0, 1000000000000):  # effectively infinite
         part = damage / (2 ** (iteration * (num_people) + player_instance))
         # equation: sum_over_iteration((damage/2^(iteration * (num_people + 1) + player_instance)))
+        if rounding:
+            part = math.floor(part)
         total += part
         if part == 0:  # don't waste computer time
             print("")
             break
-        elif iteration % 20 == 0:
+        elif part >= 1 and iteration <= 10:
             print(part, end=" | ")
 
     group_total += total  # add total damage across all players
 
     print("Total damage to player " + str(player_instance) + ": " + str(total) + "\n")
 
-print("Group total = " + str(group_total))
+print("Group total: " + str(group_total))
+print("Damage mitigated: " + str(damage - group_total))
 
 os.system("pause")
